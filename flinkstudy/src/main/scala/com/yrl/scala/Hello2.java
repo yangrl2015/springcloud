fee6ee70e706d705ab51f27a6e1cc83cbdabde1f
yangrl2015 <yangrl2014@163.com>
2020-09-20 16:50:42 +0800

    修改了Hello2

--- flinkstudy/src/main/scala/com/yrl/scala/Hello2.java
+++ flinkstudy/src/main/scala/com/yrl/scala/Hello2.java
@@ -1,10 +1,13 @@
 package com.yrl.scala;
 
+import static jdk.nashorn.internal.objects.Global.print;
+
 public class Hello2 {
     public String name;
     public Integer age;
     public Hello2(String name, Integer age){
         this.age = age;
         this.name = name;
+        print("hello");
     }
 }
